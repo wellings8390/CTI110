089cf8100204e692c0878c91154c2d0edcd85a38
wellings8390 <wellings8390@student.faytechcc.edu>
2025-03-24 04:38:58 -0400

    Add files via upload

---
+++ P1LAB1_WellingtonSamantha.py
@@ -0,0 +1,8 @@
+#Samantha Wellington
+#03/24/2025
+#P1LAB1
+#This program gets input from the user and displays output
+
+firstName = input("Enter your first name: ")
+lastName = input ("Enter your last name: ")
+print("Hello,",firstName, lastName + "! Welcome to CTI-110")
